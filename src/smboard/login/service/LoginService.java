1e7e24cd3146f96d3d1e726f48a5352358b3435a
JAYS\Jay <jwelisabeth@gmail.com>
2017-10-25 20:59:33 +0900

    login 기능 완료

---
+++ src/smboard/login/service/LoginService.java
@@ -0,0 +1,20 @@
+package smboard.login.service;
+
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import smboard.login.dao.LoginDao;
+import smboard.login.model.LoginSessionModel;
+
+public class LoginService implements LoginDao {
+	
+		private SqlMapClientTemplate sqlMapClientTemplate;
+		
+		public void setSqlMapClientTemplate(SqlMapClientTemplate sqlMapClientTemplate) {
+				this.sqlMapClientTemplate = sqlMapClientTemplate;
+		}
+		
+		@Override
+		public LoginSessionModel checkUserId(String userId){
+				return (LoginSessionModel) sqlMapClientTemplate.queryForObject("login.loginCheck", userId);
+		}
+
+}
